85e116e3b572e39618dc3652442f2ac15a8aad19
e.andreeva <e.andreeva@marketfinance.com>
2022-03-06 22:15:33 +0300

    add heading component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import {Link, Switch, Route} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/hompage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div> 
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
